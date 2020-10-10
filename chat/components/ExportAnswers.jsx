125e2bbe83ba37e084572c3a8893b7adfe7d858d
E. Krebs <ekrebs@outlook.com>
2020-10-10 17:39:14 -0500

    csv export added

--- chat/components/ExportAnswers.jsx
+++ chat/components/ExportAnswers.jsx
@@ -24,6 +24,7 @@ export function ExportAnswers(){
 						name
 						vanid
 						completed
+						doNotContact
 						answers{
 							id
 							answerText
@@ -55,11 +56,80 @@ export function ExportAnswers(){
 		
 		<div>
 			{script!==null&&(
+				<>
+				<DownloadCSVLink script={script}/>
 				<AnswerTable script={script}/>
+				</>
 			)}
 		
 	</div>);
 }
+function DownloadCSVLink(props){
+
+
+	const makeCSV=()=>{
+		let userheaders=['vanid','name','doNotContact']
+		let questionHeaders=props.script.questions.map((q)=>{
+			return q.headerName;
+		});
+
+		let fieldRows=props.script.contacts.filter((c)=>{
+			return c.completed;
+		}).map((c)=>{
+			let cells=[c.vanid,c.name,c.doNotContact?'true':'false']
+			for(let q of props.script.questions){
+				let ca=c.answers.filter((a)=>{
+					return a.question.id===q.id;
+				});
+				if(ca.length!==0){
+					cells.push(ca.map((a)=>{
+						return a.answerText;	
+					}).join('\n\n'));
+				}
+				else{
+					cells.push('')
+				}
+
+			}
+			return cells;
+		});
+		let table=[userheaders.concat(questionHeaders)]
+		if(fieldRows.length!==0){
+			table=table.concat(fieldRows);
+		}
+		console.log(table);
+
+		let csv=table.map((row)=>{
+			return row.map((cell)=>{
+				let cellSafe="";
+				//avoid injection attack
+				let cellIsNotSafe=cell.startsWith('=')||cell.startsWith('+')||cell.startsWith('-')||cell.startsWith('@');
+				if(cellIsNotSafe){
+					cellSafe=' '+cell;
+				}
+				else{
+					cellSafe=cell;
+				}
+				return '"'+cellSafe.replaceAll('"','""')+'"'
+			}).join(',')
+		}).join('\r\n');
+		console.log(csv);
+		let fname=props.script.name+'-export-results'+'.csv'
+		let file=new File([csv],fname,{
+			type:'text/csv'
+		})
+		//console.log(file);
+		return file;
+
+	}
+	let csvfile=makeCSV();
+
+
+	return <a download={csvfile.name} href={URL.createObjectURL(csvfile)} >Download CSV</a>
+}
+
+
+
 function AnswerTable(props){
 
 	// let headers=props.script.questions.map((el)=>{
@@ -74,16 +144,18 @@ function AnswerTable(props){
 			<tr>
 				<td>vanid</td>
 				<td>name</td>
+				<td>Do not Contact</td>
 				{props.script.questions.map((el)=>{
 					return <td key={el.id}> {el.headerName}</td>;
 				})}
+				<td>Chat</td>
 			</tr>
 		</thead>
 		<tbody>
 			{props.script.contacts.filter((el)=>{
 				return el.completed;
 			}).map((el)=>{
-				return <AnswerRow script={props.script} contact={el}/>
+				return <AnswerRow key={el.id} script={props.script} contact={el}/>
 			})}
 		</tbody>
 	</table>)
@@ -94,6 +166,7 @@ function AnswerRow(props){
 	return (<tr>
 		<td>{props.contact.vanid}</td>
 		<td>{props.contact.name}</td>
+		<td>{props.contact.doNotContact?'true':'false'}</td>
 		{props.script.questions.map((q)=>{
 			return (<td key={q.id}>
 				{props.contact.answers.filter((el)=>{
@@ -103,6 +176,11 @@ function AnswerRow(props){
 				}).join(',')}
 			</td>);
 		})}
+		<td>
+			<a href={'/chat?script='+encodeURIComponent(props.script.id)+'&contact='+encodeURIComponent(props.contact.id)}>
+				View
+			</a> 
+		</td>
 	</tr>);
 }
 
